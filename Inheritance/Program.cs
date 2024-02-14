c59f5eb0e2a83f8a1740e8e3f5e68097d39fe2a6
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-02-14 17:53:21 +0300

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person[] persons = new Person[3]
+        {
+            new Customer
+            {
+                FirstName = "Engin"
+            },
+            new Student
+            {
+                FirstName = "Abdulhakim"
+            },
+            new Person
+            {
+                FirstName = "Furkan"
+            }
+        };
+
+        foreach (var person in persons)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
+
+// class'ların tek başına bir anlamı vardır fakat interface'lerin tek başına bir anlamı yoktur
+class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+// bir class birden fazla class'ı inherit edemez ama birden fazla interface'i implement edebilir
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+class Student : Person
+{
+    public string Department { get; set; }
+}
